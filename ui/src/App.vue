fe93f076d66b855dcfdbf0cde4b01dabd3b7deca
Matt Gabrenya <matt@buildyourweb.app>
2023-02-06 10:52:41 -0700

    refactor: extract HomeNavbar into it's own component

--- ui/src/App.vue
+++ ui/src/App.vue
@@ -3,30 +3,7 @@
       <span class="h-16 w-16 block rounded-full border-t-4 border-white-300 animate-spin z-[10]"></span>
     </div>
     <div v-else class="h-screen">
-      <div class="navbar bg-base-300 py-4">
-        <div class="flex-1">
-          <RouterLink to="/" class="mx-4 btn btn-ghost normal-case text-3xl">
-            herddit
-          </RouterLink>
-        </div>
-        <div>
-          <ul class="menu menu-horizontal px-1 flex items-center">
-            <label htmlFor="join-herd-modal" className="btn btn-ghost">Join Secret Herd</label>
-            <RouterLink :to="`/herds/create`" class="btn btn-ghost btn-sm">Gather a Herd</RouterLink>
-
-            <li tabIndex={0}>
-              <RouterLink to="/account">
-                My Account
-                <svg class="fill-current" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z"/></svg>
-              </RouterLink>
-              <ul class="p-2 bg-base-100 z-40">
-                <li><a>Submenu 1</a></li>
-                <li><a>Submenu 2</a></li>
-              </ul>
-            </li>
-          </ul>
-        </div>
-      </div>
+      <HomeNavbar />
 
       <RouterView></RouterView>
     </div>
@@ -34,7 +11,7 @@
   <input type="checkbox" id="join-herd-modal" v-model="joinHerdModalVisible" className="modal-toggle" />
   <label htmlFor="join-herd-modal" className="modal cursor-pointer">
     <label className="modal-box relative" htmlFor="">
-      <h3 class="text-xl">Enter Secret Herd Word:</h3>
+      <h3 class="text-xl">Enter Secret Herd-Word:</h3>
       <mwc-textarea class="w-full h-32 my-4" v-model="herd_password" outlined></mwc-textarea>
       <div class="modal-action">
         <button class="btn btn-primary bn-sm" @click="joinPrivateHerd">Join Secret Herd</button>
@@ -46,9 +23,11 @@
 import { defineComponent, computed } from 'vue';
 import { AppWebsocket, ActionHash, AppAgentClient, AppAgentWebsocket, decodeHashFromBase64 } from '@holochain/client';
 import '@material/mwc-circular-progress';
+import HomeNavbar from './components/HomeNavbar.vue';
 
 export default defineComponent({
   components: {
+    HomeNavbar,
   },
   data(): {
     client: AppAgentClient | undefined;
---
+++ ui/src/components/HomeNavbar.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="navbar bg-base-300 py-4">
+    <div class="flex-1">
+      <RouterLink to="/" class="mx-4 btn btn-ghost normal-case text-3xl">
+        herddit
+      </RouterLink>
+    </div>
+    <div>
+      <ul class="menu menu-horizontal px-1 flex items-center">
+        <label htmlFor="join-herd-modal" className="btn btn-ghost">Join Secret Herd</label>
+        <RouterLink :to="`/herds/create`" class="btn btn-ghost btn-sm">Gather a Herd</RouterLink>
+
+        <li tabIndex={0}>
+          <RouterLink to="/account">
+            My Account
+            <svg class="fill-current" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z"/></svg>
+          </RouterLink>
+          <ul class="p-2 bg-base-100 z-40">
+            <li><a>Submenu 1</a></li>
+            <li><a>Submenu 2</a></li>
+          </ul>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
