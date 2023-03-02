d67b3331c43b0c21bcbeeed1d3d860f9e57d7bdc
Matt Gabrenya <matt@buildyourweb.app>
2023-03-02 16:21:24 -0700

    fix(ui): quirks in menu dropdown, move theme picker to top-level of navbar

--- ui/src/components/BaseThemeSelect.vue
+++ ui/src/components/BaseThemeSelect.vue
@@ -1,6 +1,8 @@
 <template>
-  <div class="dropdown dropdown-hover dropdown-side dropdown-left">
-    <slot>
+  <div class="dropdown dropdown-hover dropdown-bottom dropdown-end">
+    <label
+      tabindex="0"
+    >
       <svg
         xmlns="http://www.w3.org/2000/svg"
         width="32"
@@ -10,11 +12,10 @@
         fill="currentColor"
         d="M228.2 153L209 100.3a16 16 0 0 0-20.5-9.5l-59.9 21.8l11-62.8a16 16 0 0 0-2.6-12a16.2 16.2 0 0 0-10.3-6.6l-55.2-9.7a16.1 16.1 0 0 0-18.5 13L28.8 171.7a45 45 0 0 0 7.7 33.9a43.4 43.4 0 0 0 28.7 17.9a51.6 51.6 0 0 0 6.7.5H212a16 16 0 0 0 16-16v-43.7a15.7 15.7 0 0 0 .2-11.3ZM72 192a12 12 0 1 1 12-12a12 12 0 0 1-12 12Zm140 16h-88.3l88.3-32.1Zm-98.5-13.3a41 41 0 0 0 1.8-7.1l10.1-56.8l68.6-25l19.1 52.6Z"
       /></svg>
-      Theme
-    </slot>
+    </label>
     <ul
       tabindex="0"
-      class="z-40 px-0 dropdown-content menu shadow-md text-base-content rounded-box bg-base-100 max-h-64 overflow-scroll flex-nowrap"
+      class="dropdown-content menu z-40 px-0 shadow-md  bg-base-100 text-base-content rounded-box max-h-64 overflow-scroll flex-nowrap"
     >
       <li
         v-for="theme in store.themesDisplay"
--- ui/src/components/HomeNavbar.vue
+++ ui/src/components/HomeNavbar.vue
@@ -17,7 +17,7 @@
       </div>
       <ul
         v-else
-        class="menu menu-horizontal px-1 flex items-center"
+        class="menu menu-horizontal px-1 flex items-center space-x-4"
       >
         <RouterLink
           :to="`/herds/create`"
@@ -26,36 +26,32 @@
           Gather a Herd
         </RouterLink>
 
-        <li tabIndex="{0}">
-          <div class="flex flex-row justify-center">
-            <AgentProfile
-              :agentPubKey="client.myPubKey"
-              :hoverForDetails="false"
-              size="lg"
-            />
-            <svg
-              class="fill-current"
-              xmlns="http://www.w3.org/2000/svg"
-              width="20"
-              height="20"
-              viewBox="0 0 24 24"
-            ><path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" /></svg>
-          </div>
-          <ul class="z-40 bg-base-200 text-base-content shadow-md">
-            <li>
-              <BaseThemeSelect>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="32"
-                  height="32"
-                  viewBox="0 0 256 256"
-                ><path
-                  fill="currentColor"
-                  d="M228.2 153L209 100.3a16 16 0 0 0-20.5-9.5l-59.9 21.8l11-62.8a16 16 0 0 0-2.6-12a16.2 16.2 0 0 0-10.3-6.6l-55.2-9.7a16.1 16.1 0 0 0-18.5 13L28.8 171.7a45 45 0 0 0 7.7 33.9a43.4 43.4 0 0 0 28.7 17.9a51.6 51.6 0 0 0 6.7.5H212a16 16 0 0 0 16-16v-43.7a15.7 15.7 0 0 0 .2-11.3ZM72 192a12 12 0 1 1 12-12a12 12 0 0 1-12 12Zm140 16h-88.3l88.3-32.1Zm-98.5-13.3a41 41 0 0 0 1.8-7.1l10.1-56.8l68.6-25l19.1 52.6Z"
-                /></svg>
-                Theme
-              </BaseThemeSelect>
-            </li>
+        <BaseThemeSelect class="px-4" />
+
+        <div class="dropdown dropdown-hover">
+          <label
+            tabindex="0"
+            class="normal-case"
+          >
+            <div class="flex flex-row items-center justify-center space-x-2">
+              <AgentProfile
+                :agentPubKey="client.myPubKey"
+                :hoverForDetails="false"
+                size="lg"
+              />
+              <svg
+                class="fill-current"
+                xmlns="http://www.w3.org/2000/svg"
+                width="20"
+                height="20"
+                viewBox="0 0 24 24"
+              ><path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" /></svg>
+            </div>
+          </label>
+          <ul
+            tabindex="0"
+            class="dropdown-content menu z-40 bg-base-200 text-base-content shadow-md"
+          >
             <li>
               <RouterLink :to="`/agents/${myPubKeyBase64}`">
                 <svg
@@ -71,7 +67,7 @@
               </RouterLink>
             </li>
           </ul>
-        </li>
+        </div>
       </ul>
     </div>
   </div>
