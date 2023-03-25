4d672f7999a89d14e59c6fa4dd2a6f059a05a47a
Matt Gabrenya <matt@buildyourweb.app>
2023-03-25 13:38:44 -0700

    refactor(ui): use composition api in App & HomeNavbar, remove overly complex handling of profile

--- ui/src/App.vue
+++ ui/src/App.vue
@@ -12,14 +12,12 @@
   >
     <profiles-context :store="profilesStore">
       <HomeNavbar
-        :profile="profile"
         class="bg-neutral text-neutral-content"
       />
 
       <profile-prompt
         :class="{'min-h-screen w-full flex justify-center items-center': !profile, 
                  'min-h-screen w-full': profile}"
-        @profile-created="createProfile"
       >
         <RouterView />
       </profile-prompt>
@@ -37,110 +35,65 @@
     </profiles-context>
   </div>
 </template>
-<script lang="ts">
-import { defineComponent, computed } from 'vue';
-import { AppAgentClient, AppAgentWebsocket } from '@holochain/client';
+<script lang="ts" setup>
+import { provide, ref, toRaw } from 'vue';
+import { AppAgentWebsocket } from '@holochain/client';
 import HomeNavbar from './components/HomeNavbar.vue';
 import { ProfilesStore, ProfilesClient, Profile } from "@holochain-open-dev/profiles";
 import { toast } from 'vue3-toastify';
 import BaseSpinner from './components/BaseSpinner.vue';
 import { useThemeStore } from './stores/theme';
 
-export default defineComponent({
-  components: {
-    HomeNavbar,
-    BaseSpinner,
-  },
-  provide() {
-    return {
-      client: computed(() => this.client),
-      profilesStore: computed(() => this.profilesStore)
-    };
-  },
-  setup() {
-    const themeStore = useThemeStore();
+// Apply active theme
+const themeStore = useThemeStore();
+themeStore.apply(); 
 
-    return { themeStore };
-  },
-  data(): {
-    client?: AppAgentClient;
-    profilesStore?: ProfilesStore;
-    loading: boolean;
-    herd_password: string;
-    joinHerdModalVisible: boolean;
-    profile?: Profile
-  } {
-    return {
-      client: undefined,
-      profilesStore: undefined,
-      loading: true,
-      herd_password: "",
-      joinHerdModalVisible: false,
-      profile: undefined,
-    };
-  },
-  async mounted() {   
-    // Apply active theme
-    this.themeStore.apply(); 
+const client = ref<AppAgentWebsocket>();
+const profilesStore = ref();
+const loading = ref(true);
+const profile = ref<undefined | Profile>();
 
-    try {
-      // Setup conductor websocket
-      const client = await AppAgentWebsocket.connect('', 'herddit', 60000);
+provide('client', client);
+provide('profilesStore', profilesStore);
 
-      // Setup profiles store
-      const profilesClient = new ProfilesClient(client, 'directory', 'profiles');
-      this.profilesStore = new ProfilesStore(profilesClient, {
-        avatarMode: "avatar-required",
-          // Custom app level profile fields
-        additionalFields: [
-          {
-            name: "location",
-            label: "Location",
-            required: false, 
-          },
-          {
-            name: "bio",
-            label: "Bio",
-            required: false,
-          },
-          {
-            name: "website",
-            label: "Website",
-            required: false,
-          }
-        ], 
-      });
-      this.client = client;
+const setupConductor = async () => {
+  try {
+    // Setup conductor websocket
+    client.value = await AppAgentWebsocket.connect('', 'herddit', 60000);
 
-      await this.setProfile();
-
-      this.profilesStore.myProfile.subscribe((data) => {
-        if (data.status === 'complete') {
-          this.profile = data.value;
+    // Setup profiles store
+    const profilesClient = new ProfilesClient(toRaw(client.value), 'directory', 'profiles');
+    profilesStore.value = new ProfilesStore(profilesClient, {
+      avatarMode: "avatar-required",
+        // Custom app level profile fields
+      additionalFields: [
+        {
+          name: "location",
+          label: "Location",
+          required: false, 
+        },
+        {
+          name: "bio",
+          label: "Bio",
+          required: false,
+        },
+        {
+          name: "website",
+          label: "Website",
+          required: false,
         }
-      });
-      this.loading = false;
-    } catch (e: any) {
-      toast.error("Error setting up conductor websocket", e)
-    }
+      ], 
+    });
 
-  },
-  methods: {
-    async createProfile() {
-      try {
-        await this.setProfile();
-      } catch(e: any) {
-        toast.error('Error creating profile', e);
-      }
-    },
-    async setProfile() {
-      if(!this.client) return;
+    profilesStore.value.myProfile.subscribe((data: any) => {
+      if (data.status === 'complete') profile.value = data.value;
+    });
 
-      const profile = await this.profilesStore?.client.getAgentProfile(this.client.myPubKey);
-      if(profile) {
-        this.profile = profile;
-      }
-    }
-  },
-});
+    loading.value = false;
+  } catch(e) {
+    toast.error(`Error setting up conductor websocket ${e}`);
+  }
+};
+
+setupConductor();
 </script>
